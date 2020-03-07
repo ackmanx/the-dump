6d4fa732161db1dbe8ac1ee4df5c384cccc98534
Eric Majerus <ackmanx@users.noreply.github.com>
2020-03-07 15:03:20 -0600

    added prettier

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
+import React from 'react'
 import Head from 'next/head'
 
 const Home = () => (
     <div id="root">
         <Head>
             <title>the dump</title>
-            <link rel="icon" href="/favicon.ico"/>
+            <link rel="icon" href="/favicon.ico" />
         </Head>
 
         <main>the dump</main>
