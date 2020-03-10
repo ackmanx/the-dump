e5bbc0a647b87459987a3d9fd924679b1914bd2a
Eric Majerus <eric.majerus@target.com>
2020-03-10 16:15:59 -0500

    fetching from db

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,20 @@
 import React from 'react'
+import fetch from 'isomorphic-unfetch'
 import Head from 'next/head'
 
-const Home = () => (
+const Root = ({ dumpsFromDB }) => (
     <div id="root">
         <Head>
             <title>the dump</title>
             <link rel="icon" href="/favicon.ico" />
         </Head>
-
-        <main>the dump</main>
+        <main>{JSON.stringify(dumpsFromDB)}</main>
     </div>
 )
 
-export default Home
+Root.getInitialProps = async () => {
+    const res = await fetch('http://me:3000/api/dumps')
+    return { dumpsFromDB: await res.json() }
+}
+
+export default Root
