5af3393f4a08f4f841bf715031cd121c71647786
Eric Majerus <ackmanx@users.noreply.github.com>
2020-03-10 19:06:12 -0500

    put in a dummy login page and some global css

---
+++ components/Login/Login.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const styles = {
+    loginOverlay: {
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        fontSize: '90px',
+        color: 'black',
+    },
+    flexPositioner: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+    },
+    login: {
+        border: 'none',
+        outline: 'none',
+        padding: 0,
+    }
+}
+
+export const Login = () => {
+    return (
+        <div style={styles.loginOverlay}>
+            <div style={styles.flexPositioner}>
+                <input placeholder='why?' style={styles.login}/>
+            </div>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,17 @@
 import React from 'react'
-import fetch from 'isomorphic-unfetch'
 import Head from 'next/head'
 
-const Root = ({ dumpsFromDB }) => (
-    <div id="root">
+import { Login } from '../components/Login/Login'
+
+const Root = () => (
+    <div id='root'>
         <Head>
             <title>the dump</title>
-            <link rel="icon" href="/favicon.ico" />
+            <link rel='icon' href='/favicon.ico' />
         </Head>
-        <main>{JSON.stringify(dumpsFromDB)}</main>
+
+        <Login />
     </div>
 )
-
-Root.getInitialProps = async () => {
-    const res = await fetch('http://me:3000/api/dumps')
-    return { dumpsFromDB: await res.json() }
-}
 
 export default Root
