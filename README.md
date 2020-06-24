0b688cc000f6b3b8dfb922cd24c2cacace55d687
Eric Majerus <ackmanx@users.noreply.github.com>
2020-06-24 17:07:15 -0500

    misc

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-the dump
+# the dump
+
+Simple website that allows a collection of notes or code snippets to be stored
+
+
+# Development
+
+Key technologies used:
+1. Next.js
+1. TypeScript
+1. React
+1. Styled Components
+
+To start:
+Nothing special is needed to use. Next.js comes pre-built supporting TypeScript so all you do is the usual `yarn start` or equivalent
--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,12 @@
+/*
+ * The reason this file is strangely named _app.js
+ *
+ * Next.js uses the App component to initialize pages. You can override it and control the page initialization
+ * To override the default App, create the file ./pages/_app.js
+ *
+ * https://nextjs.org/docs/advanced-features/custom-app
+ */
+
 import React from 'react'
 import { createGlobalStyle } from 'styled-components'
 
